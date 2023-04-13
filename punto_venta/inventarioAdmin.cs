3564262c864fa4bed174b648f942180f7ee9c526
Abdiel <abdiel2012p@gmail.com>
2023-04-12 22:32:12 -0600

    Plantilla de la vista del administrador para el inventario agregada

---
+++ punto_venta/inventarioAdmin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace punto_venta
+{
+    public partial class inventarioAdmin : Form
+    {
+        public inventarioAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void tbBuscar_enter(object sender, EventArgs e)
+        {
+            if (tbBuscar.Text == "Ingrese nombre, categoría o descripción")
+            {
+                tbBuscar.Text = "";
+                tbBuscar.ForeColor = Color.Black;
+            }
+        }
+
+        private void tbBuscar_leave(object sender, EventArgs e)
+        {
+            if (tbBuscar.Text == "")
+            {
+                tbBuscar.Text = "Ingrese nombre, categoría o descripción";
+                tbBuscar.ForeColor = Color.Silver;
+            }
+        }
+   
+    }
+}
